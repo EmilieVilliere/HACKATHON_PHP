a7861a2fc445f9e628117ae5e6096e946f75571f
EmilieVilliere <you@example.com>
2019-04-18 19:12:52 +0200

    Add design

--- oeuf.php
+++ oeuf.php
@@ -31,31 +31,27 @@ $dataEgg = $reponseEgg->fetchAll();
                 <title>Document</title>
             </head>
             <body>
-                <div class="card-deck">
-                    <a href=<?= "oeuf2.php?power=".$dataEgg[0]['power']; ?> >
-                        <div class="card">
+                <div class="container">
+                        <div class="card-deck">
+                        <a class="card" href=<?= "oeuf2.php?power=".$dataEgg[0]['power']; ?> >
                             <img class="card-img-top" src="<?= $dataEgg[0]['image']; ?>" alt="<?php echo $dataEgg[0]['name']; ?>" >
                             <div class="card-body">
                                 <h5 class="card-title"><?= $dataEgg[0]['name']; ?></h5>
                             </div>
-                        </div>
-                    </a>
-                    <a href=<?= "oeuf2.php?power=".$dataEgg[1]['power']; ?> >
-                        <div class="card">
+                        </a>
+                        <a class="card" href=<?= "oeuf2.php?power=".$dataEgg[1]['power']; ?> >
                             <img class="card-img-top" src="<?= $dataEgg[1]['image']; ?>" alt="<?php echo $dataEgg[1]['name']; ?>" >
                             <div class="card-body">
                                 <h5 class="card-title"><?= $dataEgg[1]['name']; ?></h5>
                             </div>
-                        </div>
-                    </a>
-                    <a href=<?= "oeuf2.php?power=".$dataEgg[2]['power']; ?> >
-                        <div class="card">
+                        </a>
+                        <a class="card" href=<?= "oeuf2.php?power=".$dataEgg[2]['power']; ?> >
                             <img class="card-img-top" src=<?= $dataEgg[2]['image']; ?> alt=<?php echo $dataEgg[1]['name']; ?> >
                             <div class="card-body">
                                 <h5 class="card-title"><?= $dataEgg[2]['name']; ?></h5>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </div>
             </body>
         </html>
