8dd148f6f32175b73b1cecc5fb5d6bae3b1ecac9
florian Logez <flo.log09@gmail.com>
2019-04-18 18:10:02 +0200

    place img

--- page_personnages.php
+++ page_personnages.php
@@ -1,7 +1,7 @@
 <?php
 
-$connection = new PDO("mysql:dbname=easter_eggs_hackaton;host=localhost", "root", "lOMELANGEE0803");
-$requete ="SELECT name, image, skills FROM characters ORDER BY RAND() LIMIT 7;";
+$connection = new PDO("mysql:dbname=easter_eggs_hackaton;host=localhost", "root", "123");
+$requete ="SELECT name, image, skills FROM characters ORDER BY RAND() LIMIT 9;";
 $charactersInfo = $connection->query($requete);
 $resultat = $charactersInfo->fetchAll();
 echo "<pre>";
@@ -20,61 +20,99 @@ echo "</pre>";
         </head>
         <html>
         <body>
+            <div class="container">
 
         <!-- Les cards de nos oeufs apparaissent ici -->
-            <div class="card-columns">
-            <div class="card">
-                <div class="card-body">
-                <img src=<?php echo $resultat[0]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[0]["name"]; ?></h4>
-                <p class="card-description"><?php echo $resultat[0]["skills"]; ?></p>
-                <!-- Card content -->
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[0]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[0]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[1]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[1]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[2]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[2]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[3]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[3]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[4]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[4]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[5]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[5]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[6]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[6]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[7]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[7]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src=<?php echo $resultat[8]["image"]; ?>>
+                            <div class="card-body">
+                            <h4 class="card-title"><?php echo $resultat[8]["name"]; ?></h4>
+                            <!-- Card content -->
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card p-3">
-            <img src=<?php echo $resultat[1]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[1]["name"]; ?></h4>
-                <!-- Card content -->
-            </div>
-            <div class="card">
-                <div class="card-body">
-                <img src=<?php echo $resultat[2]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[2]["name"]; ?></h4>
-                <!-- Card content -->
-                </div>
-            </div>
-            <div class="card p-3">
-            <img src=<?php echo $resultat[3]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[3]["name"]; ?></h4>
-                <!-- Card content -->
-            </div>
-            <div class="card">
-                <div class="card-body">
-                <img src=<?php echo $resultat[4]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[4]["name"]; ?></h4>
-                <!-- Card content -->
-                </div>
-            </div>
-            <div class="card p-3">
-            <img src=<?php echo $resultat[5]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[5]["name"]; ?></h4>
-                <!-- Card content -->
-            </div>
-            <div class="card">
-                <div class="card-body">
-                <img src=<?php echo $resultat[6]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[6]["name"]; ?></h4>
-                <!-- Card content -->
-                </div>
-            </div>
-            <div class="card p-3">
-            <img src=<?php echo $resultat[7]["image"]; ?>>
-                <h4 class="card-title"><?php echo $resultat[7]["name"]; ?></h4>
-                <!-- Card content -->
-            </div>
-            </div>
-        </html>
         </body>
+    </html>
 
 <!-- Appel du Bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
